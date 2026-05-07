c8c81107a27564fffe1534611bfc0cd3debb0a71
Henry Abahenya <abahenyahadre.com>
2026-05-07 13:30:52 +0300

    Final fix: forcing dist folder to be tracked for Render

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const app = express()
 // Minimal necessary middlewares
 app.use(cors())
 app.use(express.json())
+
 app.use(express.static('dist'))
 
 let notes = [
@@ -18,9 +19,7 @@ app.get('/', (req, res) => {
   res.json(notes)
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
+
 
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id
